0406a0f09ffc4b17677d861e846e45d8c2f961b3
Guillermo <guillermojoel.huerta@gmail.com>
2023-07-02 23:05:00 -0600

    Se agrega lista simplemente enlazada, inserción al final

---
+++ src/_10_ListasSimplementeEnlazadas_InsersionAlFinal/App.java
@@ -0,0 +1,57 @@
+package _10_ListasSimplementeEnlazadas_InsersionAlFinal;
+
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args){
+        Lista listita = new Lista();
+        int opcion = 0, el;
+        do{
+            try{
+                opcion = Integer.parseInt(JOptionPane.showInputDialog(null,
+                        "1. Agregar un elemento al inicio de la lista\n" +
+                                "2. Agregar un elemento al final de la lista\n" +
+                                "3. Mostrar los datos de la lista\n" +
+                                "4. Salir\n", "Menú de opciones",3));
+
+                switch(opcion){
+                    case 1:
+                        try{
+                            el = Integer.parseInt(JOptionPane.showInputDialog(null,
+                                    "Ingresa el elemento",
+                                    "Insertando al inicio",
+                                    3));
+                            //Agregando al nodo
+                            listita.agregarAlInicio(el);
+                        }catch(NumberFormatException n){
+                            JOptionPane.showMessageDialog(null, "Error " + n.getMessage());
+                        }
+                        break;
+                    case 2:
+                        try{
+                            el = Integer.parseInt(JOptionPane.showInputDialog(null,
+                                    "Ingresa el elemento",
+                                    "Insertando al final",
+                                    3));
+                            //Agregando al nodo
+                            listita.agregarAlFinal(el);
+                        }catch(NumberFormatException n){
+                            JOptionPane.showMessageDialog(null, "Error " + n.getMessage());
+                        }
+                        break;
+                    case 3:
+                        listita.mostrarLista();
+                        break;
+                    case 4:
+                        JOptionPane.showMessageDialog(null, "Programa finalizado");
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "0pción incorrecta");
+
+                }
+            }catch(Exception e){
+                JOptionPane.showMessageDialog(null, "Error " + e.getMessage());
+            }
+        }while(opcion!=4);
+    }
+}
---
+++ src/_10_ListasSimplementeEnlazadas_InsersionAlFinal/Lista.java
@@ -0,0 +1,44 @@
+package _10_ListasSimplementeEnlazadas_InsersionAlFinal;
+
+public class Lista {
+    protected Nodo inicio, fin; //Punteros para saber donde esta el inicio y final
+
+    public Lista(){
+        inicio = null;
+        fin = null;
+    }
+    public boolean estaVacia(){
+        if(inicio == null){
+            return true;
+        }else {
+            return false;
+        }
+    }
+    //método para agregar un nodo al inicio de la lista
+    public void agregarAlInicio(int elemento){
+        //Creando al nodo
+        inicio = new Nodo(elemento, inicio);
+        if(fin == null) {
+            fin = inicio;
+        }
+    }
+
+    public void agregarAlFinal(int elemento){
+        if(!estaVacia()){
+            fin.siguiente = new Nodo(elemento);
+            fin = fin.siguiente;
+        }else{
+         inicio = fin = new Nodo(elemento);
+        }
+    }
+
+    //Método para mostrar los datos
+    public void mostrarLista(){
+        Nodo recorrer = inicio;
+        System.out.println();
+        while(recorrer!=null){
+            System.out.print("["+recorrer.dato+"]--->");
+            recorrer = recorrer.siguiente;
+        }
+    }
+}
